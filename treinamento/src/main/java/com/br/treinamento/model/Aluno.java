f2a109d1779c92a74ae91a2e0ad307583465a901
lnrdgst <leo.afm@hotmail.com>
2021-12-02 14:16:33 -0300

    deve alterar apenas a classe Aluno

--- treinamento/src/main/java/com/br/treinamento/model/Aluno.java
+++ treinamento/src/main/java/com/br/treinamento/model/Aluno.java
@@ -1,5 +1,59 @@
 package com.br.treinamento.model;
 
+import java.util.Objects;
+
 public class Aluno {
+	
+	private Long id;
+	
+	private String matricula;
+	
+	private String nomeCompleto;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+
+	public String getNomeCompleto() {
+		return nomeCompleto;
+	}
+
+	public void setNomeCompleto(String nomeCompleto) {
+		this.nomeCompleto = nomeCompleto;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Aluno other = (Aluno) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+	
+	
+
 
 }
